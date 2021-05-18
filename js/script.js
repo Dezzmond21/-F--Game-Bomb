d8c5a04d250d64bfdb29d223cf37017dac5081ee
Nicolas Klaimyk <46502035+Dezzmond21@users.noreply.github.com>
2021-05-18 17:22:10 +0300

    master

--- js/script.js
+++ js/script.js
@@ -1,4 +1,49 @@
+var getRandomNumber = function (size) {
+   return Math.floor(Math.random() * size);
+};
+var getDistance = function (event, target) {
+   var diffX = event.offsetX - target.x;
+   var diffY = event.offsetY - target.y;
+   return Math.sqrt((diffX * diffX) + (diffY * diffY));
+};
+var getDistanceHint = function (distance) {
+   if (distance < 10) {
+      return "Aaaaaa close!";
+   } else if (distance < 20) {
+      return "Very Hot";
+   } else if (distance < 40) {
+      return "Hot";
+   } else if (distance < 80) {
+      return "Warm";
+   } else if (distance < 160) {
+      return "Cold";
+   } else if (distance < 320) {
+      return "Very Cold";
+   } else {
+      return "Aaaaa Impossible Cold";
+   };
+};
+var clicks = 0;
+var width = 400;
+var height = 400;
 
+var target = {
+   x: getRandomNumber(width),
+   y: getRandomNumber(height)
+};
+
+$("#map").click(function (event) {
+   clicks++;
+   var distance = getDistance(event, target);
+
+   var distanceHint = getDistanceHint(distance);
+
+   $("#distance").text(distanceHint);
+
+   if (distance < 8) {
+      alert("Bomb activation. Good Work. You made it on: " + clicks + " clicks");
+   };
+});
 
 
 
